8e1b28848490e01118f3dcb005849d0d8f6351ee
imsanzay21 <imsanzay09@gmail.com>
2019-07-09 06:42:54 +0530

    components pushed

---
+++ ShoppeFront/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/_layout/header/header.component';
+import { LayoutComponent } from './components/_layout/layout/layout.component';
+import { FooterComponent } from './components/_layout/footer/footer.component';
+import { SidenavComponent } from './components/_layout/sidenav/sidenav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RequestResetComponent,
+    ResponseResetComponent,
+    HomeComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent,
+    SidenavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
